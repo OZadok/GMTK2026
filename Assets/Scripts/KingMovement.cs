54d8365c5498860b9c5de2464ceda421e7fbfd39
Omer Zadok <OZadok@gmail.com>
2026-07-23 20:30:14 +0300

    king don't move on gameover

--- Assets/Scripts/KingMovement.cs
+++ Assets/Scripts/KingMovement.cs
@@ -1,11 +1,34 @@
 using System;
+using Events;
+using SuperMaxim.Messaging;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class KingMovement : MonoBehaviour
 {
 	[SerializeField] private float _speed;
+	
+	private bool _isDead = false;
 	private void Update()
 	{
-		transform.position += transform.right * (_speed * Time.deltaTime);
+		if (!_isDead)
+		{
+			transform.position += transform.right * (_speed * Time.deltaTime);
+		}
+	}
+
+	private void OnEnable()
+	{
+		Messenger.Default.Subscribe <GameOverEvent>(OnGameOver);
+	}
+
+	private void OnDisable()
+	{
+		Messenger.Default.Unsubscribe <GameOverEvent>(OnGameOver);
+	}
+
+	private void OnGameOver(GameOverEvent obj)
+	{
+		_isDead = true;
 	}
 }
